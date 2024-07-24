6df76f7036d5db476c45f3aa089dff05768897e2
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-07-24 10:29:11 -0400

    13 of 46 completed

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,13 +1,14 @@
 import {TestBed} from '@angular/core/testing';
 import {CoursesService} from './courses.service';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { COURSES } from '../../../../server/db-data';
 
 describe("CoursesService", ()=>{
 
     let coursesService: CoursesService, 
-        HttpClientTestingModule:HttpTestingController;
+        httpTestingController:HttpTestingController;
 
-    beforeEach(()=>{
+    beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
             providers: [
@@ -21,7 +22,25 @@ describe("CoursesService", ()=>{
     });
 
     it('should retrieve all courses', ()=>{
+        coursesService.findAllCourses()
+            .subscribe(courses => {
+                
+                expect(courses).toBeTruthy();
+                
+                expect(courses.length).toBe(12,
+                    "incorrect number of courses"
+                );
+                
+                const course = courses.find( course => course.id == 12 );
 
+                expect(course.titles.description).toBe( "Angular Testing Course" );
+
+            });
+        const req = httpTestingController.expectOne('/api/courses');
+
+        expect(req.request.method).toEqual("GET");
+
+        req.flush({payload:Object.values(COURSES)});
     });
 
 });
