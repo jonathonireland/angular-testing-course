a1e69cf3d0a0dae5cc78eee5c28b698f26a4dfcc
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-07-24 18:18:13 -0400

    20 of 46 completed

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {waitForAsync, ComponentFixture, TestBed} from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
 import {COURSES} from '../../../../server/db-data';
@@ -8,24 +8,30 @@ import {sortCoursesBySeqNo} from '../home/sort-course-by-seq';
 import {Course} from '../model/course';
 import {setupCourses} from '../common/setup-test-data';
 
-
-
-
 describe('CoursesCardListComponent', () => {
 
-  beforeEach(()=>{
+  let component: CoursesCardListComponent;
+
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+
+  beforeEach(waitForAsync( ()=>{
     TestBed.configureTestingModule({
       imports: [
         CoursesModule
       ]
-    });
-   
-  });
+    })
+    .compileComponents()
+    .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+      });
+  }));
 
 
   it("should create the component", () => {
 
-   pending();
+   expect(component).toBeTruthy();
+   console.log(component);
 
   });
 
