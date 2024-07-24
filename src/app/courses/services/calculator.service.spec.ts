78d5d1ee76d0545b45dec0edee7b8cb232612856
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-07-23 22:23:22 -0400

    12 of 46 completed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,5 +1,6 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service"; 
+import { TestBed } from '@angular/core/testing';
 
 describe('CalculatorService', () => { 
 
@@ -10,7 +11,14 @@ describe('CalculatorService', () => {
 
   beforeEach(()=>{
     loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
-    calculator = new CalculatorService(loggerSpy);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {provide: LoggerService, useValue:loggerSpy}
+      ]
+    });
+    calculator = TestBed.inject(CalculatorService);
   });
   
   it('should add two numbers', () => {
