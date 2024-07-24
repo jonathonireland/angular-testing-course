6640230cf8d02115dce914fec6457375ebb469b5
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-07-24 11:42:57 -0400

    17 of 46 completed

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,7 +1,7 @@
 import {TestBed} from '@angular/core/testing';
 import {CoursesService} from './courses.service';
 import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import { COURSES } from '../../../../server/db-data';
+import { COURSES, findLessonsForCourse } from '../../../../server/db-data';
 import { Course } from '../model/course';
 import { HttpErrorResponse } from '@angular/common/http';
 
@@ -94,7 +94,7 @@ describe("CoursesService", ()=>{
         
         const changes :Partial<Course> = {titles:{description:'Testing Course'}};
 
-        coursesService.saveCourse(12, changes)
+        coursesService.saveCourse(12, changes) // FAILING TEST FOR METHOD SAVECOURSE
             .subscribe(
                 ()=> fail("the save course operation should have failed"),
                 
@@ -109,6 +109,31 @@ describe("CoursesService", ()=>{
 
         req.flush('Save course failed', {status:500, 
             statusText:'Internal Server Error'});
+    });
+
+    it('should find a list of lessons', () => {
+        
+      coursesService.findLessons(12)
+        .subscribe(lessons => {
+            expect(lessons).toBeTruthy();
+            expect(lessons.length).toBe(3);
+
+        });
+
+        const req = httpTestingController.expectOne(
+            req => req.url == '/api/lessons'
+        );
+        
+        expect(req.request.method).toEqual("GET");
+        expect(req.request.params.get("courseId")).toEqual("12");
+        expect(req.request.params.get("filter")).toEqual("");
+        expect(req.request.params.get("sortOrder")).toEqual("asc");
+        expect(req.request.params.get("pageNumber")).toEqual("0");
+        expect(req.request.params.get("pageSize")).toEqual("3");
+
+        req.flush({
+            payload: findLessonsForCourse(12).slice(0,3)
+        });
 
     });
 
