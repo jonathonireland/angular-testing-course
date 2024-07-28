32fc688e821f5480faded058eb69f6acb6f8258b
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-07-27 21:11:29 -0400

    40 of 46 completed

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,19 @@
 describe('Home Page', () => {
     it('should display a list of courses', ()=> {
-       cy.visit('/');
-       cy.contains("All Courses");
+        
+        cy.fixture('courses.json').as("coursesJSON");
+
+        cy.intercept('GET', '/api/courses').as('courses');
+
+        // cy.route("/api/courses", "@coursesJSON").as("courses");
+
+        cy.visit('/');
+
+        cy.contains("All Courses");
+
+        cy.wait('@courses');
+
+        cy.get("mat-card").should("have.length", 9);
+
     })
 });
