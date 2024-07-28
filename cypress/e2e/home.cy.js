4e977fcd29bc4d98e8709a04f11dd19d49af9eb3
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-07-28 01:46:54 -0400

    41 of 46 completed

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,19 +1,34 @@
 describe('Home Page', () => {
-    it('should display a list of courses', ()=> {
+    
+    beforeEach(()=>{
         
         cy.fixture('courses.json').as("coursesJSON");
 
         cy.intercept('GET', '/api/courses').as('courses');
 
-        // cy.route("/api/courses", "@coursesJSON").as("courses");
-
         cy.visit('/');
+    });
+    
+    // xit('should display a list of courses', ()=> {
+        
+    //     // cy.route("/api/courses", "@coursesJSON").as("courses");
 
-        cy.contains("All Courses");
+    //     cy.contains("All Courses");
 
-        cy.wait('@courses');
+    //     cy.wait('@courses');
 
-        cy.get("mat-card").should("have.length", 9);
+    //     cy.get("mat-card").should("have.length", 9);
+    // });
 
-    })
+    it('should display the advanced courses', ()=> {
+
+        cy.get('.mat-tab-label').should("have.length", 2);
+        
+        cy.get('.mat-tab-label').last().click();
+
+        cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.gt', 1);
+
+        cy.get('.mat-tab-body-active .mat-card-title').first().should('contain', "Angular Security Course");
+
+    });
 });
